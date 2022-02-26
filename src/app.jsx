09b8b66e4b56a745809c286541f11c29b51dd8b9
jin0kim326 <jin0kim326@gamil.com>
2022-02-26 17:02:50 +0900

    select video

--- src/app.jsx
+++ src/app.jsx
@@ -3,52 +3,27 @@ import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
 import SimpleSearchHeader from "./components/search_header/simple_search_header";
 import VideoList from "./components/video_list/video_list";
+import Youtube from "./service/youtube";
 
 class App extends Component {
+  youtube = this.props.youtube;
+
   state = {
     videos: [],
     clickedVideo: false,
-    searchTerm: "",
   };
-
-  onSearch = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search/?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAHz7ywb1SC3oCmi5eWD8wN2d3gYPIHgFQ`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => this.setState({ videos: items }))
-      .catch((error) => console.log("error", error));
-  };
-
-  loadPopularVideoList() {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=24&key=AIzaSyAHz7ywb1SC3oCmi5eWD8wN2d3gYPIHgFQ",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        this.setState({ videos: result.items });
-      })
-      .catch((error) => console.log("error", error));
-  }
 
   componentDidMount() {
-    this.loadPopularVideoList();
+    this.youtube
+      .loadPopularVideoList()
+      .then((videos) => this.setState({ videos }));
   }
+
+  onSearch = (query) => {
+    this.youtube
+      .onSearch(query) //
+      .then((videos) => this.setState({ videos }));
+  };
 
   render() {
     return (
--- src/components/video_detail/video_detail.jsx
+++ src/components/video_detail/video_detail.jsx
@@ -4,14 +4,7 @@ import styles from "./video_detail.module.css";
 class VideoDetail extends Component {
   render() {
     return (
-      <article className={styles.video_detail}>
-        <video src="" className={styles.video}></video>
-        <div className={styles.video_info}>
-          <span className={styles.video_title}></span>
-          <span className={styles.video_channel}></span>
-          <span className={styles.video_description}></span>
-        </div>
-      </article>
+      <section className=""></section>
     );
   }
 }
